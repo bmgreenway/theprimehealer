a60221393e5c49a1f975e56de4365906a23542d6
Michael Cook (mackal) <mcook@mackal.net>
2017-11-07 21:45:35 -0500

    Spell Cache WIP

---
+++ zone/spell_cache.cpp
@@ -0,0 +1,113 @@
+#include "spell_cache.h"
+
+void SpellCache::InsertSpellEffect(int affect, int value, int subindex)
+{
+	sEffectCache *effect = nullptr;
+
+	auto range = m_spelleffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			break;
+		++effect_iter;
+	}
+
+	if (effect_iter == range.second) {// we didn't find one
+		sEffectCache e{affect, value, subindex};
+		m_spelleffect.emplace(affect, e);
+		return;
+	}
+
+	// we gotta update
+	effect_iter->second.base1 = value;
+}
+
+void SpellCache::InsertItemEffect(int affect, int value, int subindex)
+{
+	sEffectCache *effect = nullptr;
+
+	auto range = m_itemeffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			break;
+		++effect_iter;
+	}
+
+	if (effect_iter == range.second) {// we didn't find one
+		sEffectCache e{affect, value, subindex};
+		m_itemeffect.emplace(affect, e);
+		return;
+	}
+
+	// we gotta update
+	effect_iter->second.base1 = value;
+}
+
+void SpellCache::InsertAltEffect(int affect, int value, int subindex)
+{
+	sEffectCache *effect = nullptr;
+
+	auto range = m_alteffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			break;
+		++effect_iter;
+	}
+
+	if (effect_iter == range.second) {// we didn't find one
+		sEffectCache e{affect, value, subindex};
+		m_alteffect.emplace(affect, e);
+		return;
+	}
+
+	// we gotta update
+	effect_iter->second.base1 = value;
+}
+
+const SpellCache::sEffectCache *SpellCache::GetSpellCached(int affect, int subindex)
+{
+	auto range = m_spelleffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			return &effect_iter->second;
+		++effect_iter;
+	}
+
+	return nullptr;
+}
+
+const SpellCache::sEffectCache *SpellCache::GetItemCached(int affect, int subindex)
+{
+	auto range = m_itemeffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			return &effect_iter->second;
+		++effect_iter;
+	}
+
+	return nullptr;
+}
+
+const SpellCache::sEffectCache *SpellCache::GetAltCached(int affect, int subindex)
+{
+	auto range = m_alteffect.equal_range(affect);
+	auto effect_iter = range.first;
+
+	while (effect_iter != range.second) {
+		if (effect_iter->second.base2 == subindex)
+			return &effect_iter->second;
+		++effect_iter;
+	}
+
+	return nullptr;
+}
+
---
+++ zone/spell_cache.h
@@ -0,0 +1,69 @@
+#ifndef SPELL_CACHE_H
+#define SPELL_CACHE_H
+
+#include <map>
+
+class SpellCache
+{
+public:
+	struct sEffectCache {
+		int affect;
+		int base1;
+		int base2;
+	};
+
+	SpellCache() : spell_cached(false), item_cached(false), alt_cached(false) {}
+	~SpellCache() {}
+
+	void InsertSpellEffect(int affect, int value, int subindex);
+	void InsertItemEffect(int affect, int value, int subindex);
+	void InsertAltEffect(int affect, int value, int subindex);
+
+	inline void ClearSpellEffect() { m_spelleffect.clear(); }
+	inline void ClearItemEffect() { m_itemeffect.clear(); }
+	inline void ClearAltEffect() { m_alteffect.clear(); }
+
+	inline void SetSpellCached(bool v) { spell_cached = v; }
+	inline void SetItemCached(bool v) { item_cached = v; }
+	inline void SetAltCached(bool v) { alt_cached = v; }
+
+	inline bool IsSpellCached() { return spell_cached; }
+	inline bool IsItemCached() { return item_cached; }
+	inline bool IsAltCached() { return alt_cached; }
+
+	const sEffectCache *GetSpellCached(int affect, int subindex = 0);
+	const sEffectCache *GetItemCached(int affect, int subindex = 0);
+	const sEffectCache *GetAltCached(int affect, int subindex = 0);
+
+	// inlines for common operations
+	inline int GetCachedPlayerEffect(int affect, int subindex = 0) {
+		auto res = GetSpellCached(affect, subindex);
+		if (res)
+			return res->base1;
+		return 0;
+	}
+
+	inline int GetCachedItemEffect(int affect, int subindex = 0) {
+		auto res = GetItemCached(affect, subindex);
+		if (res)
+			return res->base1;
+		return 0;
+	}
+
+	inline int GetCachedAltEffect(int affect, int subindex = 0) {
+		auto res = GetAltCached(affect, subindex);
+		if (res)
+			return res->base1;
+		return 0;
+	}
+
+private:
+	bool spell_cached;
+	bool item_cached;
+	bool alt_cached;
+	std::multimap<int, sEffectCache> m_spelleffect;
+	std::multimap<int, sEffectCache> m_itemeffect;
+	std::multimap<int, sEffectCache> m_alteffect;
+};
+
+#endif /* !SPELL_CACHE_H */
